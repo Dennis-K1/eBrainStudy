a4ef1cb01d8a7c88543b26a3f989333346af4b51
Dennis-K1 <dennis.k1.dev@gmail.com>
2022-12-14 20:40:16 +0900

    Update Article DTO/VO

---
+++ week1/web/src/classes/ArticleVO.java
@@ -0,0 +1,26 @@
+package classes;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+public class ArticleVO {
+
+    private int id;
+    private String articleCategoryName;
+    private String title;
+    private String content;
+    private int views;
+    private String writer;
+    private String password;
+    private String dateCreated;
+    private String lastUpdated;
+    private int fileAttached;
+
+    public ArticleVO() {
+
+    }
+}
