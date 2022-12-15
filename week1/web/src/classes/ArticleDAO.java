aff6561bb46c055a31eb4beaa6a7bc181f89ce46
Dennis-K1 <dennis.k1.dev@gmail.com>
2022-12-16 00:21:52 +0900

    Update getArticles Method (overload method with queryString parameters)

--- week1/web/src/classes/ArticleDAO.java
+++ week1/web/src/classes/ArticleDAO.java
@@ -2,6 +2,7 @@ package classes;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ArticleDAO {
@@ -33,6 +34,76 @@ public class ArticleDAO {
             pstmt = conn.prepareStatement(query);
             pstmt.setInt(1, pageSize);
             pstmt.setInt(2, currentPage);
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("article_id");
+                String articleCategoryName = rs.getString("article_category.name");
+                String title = rs.getString("title");
+                String content = rs.getString("content");
+                String writer = rs.getString("writer");
+                int views = rs.getInt("views");
+                String  dateCreated = rs.getString("date_created");
+                String  lastUpdated = rs.getString("last_updated");
+                if (lastUpdated == null) {
+                    lastUpdated = "-";
+                }
+                int fileAttached = rs.getInt("file_attached");
+                ArticleVO article = new ArticleVO();
+                article.setId(id);
+                article.setArticleCategoryName(articleCategoryName);
+                article.setTitle(title);
+                article.setContent(content);
+                article.setWriter(writer);
+                article.setViews(views);
+                article.setDateCreated(dateCreated);
+                article.setLastUpdated(lastUpdated);
+                article.setFileAttached(fileAttached);
+                articlesList.add(article);
+                rs.close();
+                pstmt.close();
+                conn.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("article error");
+            e.printStackTrace();
+        }
+        return articlesList;
+    }
+
+    public List getArticles(int pageSize, int currentPage, HashMap<String,String> queryStrings) {
+        try {
+            conn = DriverManager.getConnection(url,userName,password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        List<ArticleVO> articlesList = new ArrayList<>();
+        if (queryStrings.get("fromDate").equals("") && queryStrings.get("toDate").equals("")) {
+            queryStrings.put("fromDate","2000-01-01");
+            queryStrings.put("toDate","2099-01-01");
+        }
+        try {
+            query = "select article_category.name, article.* " +
+                    "from article_category " +
+                    "right outer join article " +
+                    "on article.article_category_id = article_category.article_category_id " +
+                    "where" +
+                    "    (? < date_created  and (? + INTERVAL 1 DAY) > date_created)" +
+                    "    and article_category.name LIKE ?" +
+                    "    and (title LIKE ? or writer LIKE ? or content like ?)" +
+                    "limit ? " +
+                    "offset ?";
+            pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, queryStrings.get("fromDate"));
+            pstmt.setString(2, queryStrings.get("toDate"));
+            pstmt.setString(3, queryStrings.get("category"));
+            pstmt.setString(4, queryStrings.get("query"));
+            pstmt.setString(5, queryStrings.get("query"));
+            pstmt.setString(6, queryStrings.get("query"));
+            pstmt.setInt(7, pageSize);
+            pstmt.setInt(8, currentPage);
+            System.out.println(pstmt.toString());
             rs = pstmt.executeQuery();
             while (rs.next()) {
                 int id = rs.getInt("article_id");
