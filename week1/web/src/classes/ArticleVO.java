fb4d2820f388da0ae9750ac1b306fead164d736d
Dennis-K1 <dennis.k1.dev@gmail.com>
2022-12-16 12:10:29 +0900

    Upadate articleCategoryId member variable

--- week1/web/src/classes/ArticleVO.java
+++ week1/web/src/classes/ArticleVO.java
@@ -10,6 +10,7 @@ import java.sql.Date;
 public class ArticleVO {
 
     private int id;
+    private int articleCategoryId;
     private String articleCategoryName;
     private String title;
     private String content;
