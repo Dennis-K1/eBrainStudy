ad292ac09196f044e32a864767181c39f938e1c9
Dennis-K1 <dennis.k1.dev@gmail.com>
2022-12-23 02:09:19 +0900

    Update Path to Default Context

--- week2/src/main/java/com/week2/controller/Controller.java
+++ week2/src/main/java/com/week2/controller/Controller.java
@@ -7,7 +7,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet("/control")
+@WebServlet("/")
 public class Controller extends HttpServlet {
 
 	protected void processRequest (HttpServletRequest request, HttpServletResponse response)
