d12111f2aa460564b1dd28a3938ec7a2fa6d13b4
Dennis-K1 <dennis.k1.dev@gmail.com>
2022-12-23 16:12:05 +0900

    Update Lombok and Static Final Variables (file-related)

--- week2/src/main/java/com/week2/model/FileVO.java
+++ week2/src/main/java/com/week2/model/FileVO.java
@@ -1,5 +1,10 @@
 package com.week2.model;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class FileVO {
 /*
     id         `file_id` INT(10) NOT NULL AUTO_INCREMENT  - 파일 번호
@@ -27,4 +32,16 @@ public class FileVO {
      * 파일 경로
      */
     private String path;
+
+
+    /**
+     * saveFolderName	    파일 저장 폴더
+     * fileEncType			변환 형싱
+     * fileMaxSize			파일 최대 크기
+     */
+    public static final String saveFolderName = "articleFiles";
+    public static final String fileEncType = "utf-8";
+    public static final int fileMaxSize=5*1024*1024;
+    public static final String absolutePath = "E:\\coding\\eBrainStudy\\week2\\articleFiles";
+    public static final String tempPath = "E:\\coding\\eBrainStudy\\week2\\articleFiles\\temp";
 }
