26e7c3e442d42868d609a22afe6a84acb3f2806c
Dennis-K1 <dennis.k1.dev@gmail.com>
2022-12-23 16:10:06 +0900

    Update Single File Upload - Still on going

--- week2/src/main/java/com/week2/command/InsertArticleCommand.java
+++ week2/src/main/java/com/week2/command/InsertArticleCommand.java
@@ -1,8 +1,21 @@
 package com.week2.command;
 
+
+import static com.week2.model.FileVO.absolutePath;
+import static com.week2.model.FileVO.fileEncType;
+import static com.week2.model.FileVO.fileMaxSize;
+import static com.week2.model.FileVO.tempPath;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 import com.week2.model.ArticleDAO;
 import com.week2.model.ArticleVO;
+import com.week2.model.FileDAO;
+import com.week2.model.FileVO;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,16 +27,24 @@ public class InsertArticleCommand implements Command {
 	private HttpServletRequest request;
 	private HttpServletResponse response;
 
+
+
 	public String execute() throws ServletException, IOException {
+
+		/**
+		 * tempPath			임시 저장 경로
+		 * absolutePath		파일 저장 절대 경로
+		 */
+		MultipartRequest multi = new MultipartRequest(request,tempPath,fileMaxSize,fileEncType,new DefaultFileRenamePolicy());
+
+		String fileName = multi.getFilesystemName("fileName");
+		int categoryId = Integer.parseInt(multi.getParameter("category"));
+		String title = multi.getParameter("title");
+		String content = multi.getParameter("content");
+		String writer = multi.getParameter("writer");
+		String password = multi.getParameter("password");
+
 		ArticleDAO articleDAO = new ArticleDAO();
-		request.setCharacterEncoding("utf-8");
-
-		int categoryId = Integer.parseInt(request.getParameter("category"));
-		String title = request.getParameter("title");
-		String content = request.getParameter("content");
-		String writer = request.getParameter("writer");
-		String password = request.getParameter("password");
-
 		ArticleVO article = ArticleVO.builder()
 			.categoryId(categoryId)
 			.title(title)
@@ -32,13 +53,31 @@ public class InsertArticleCommand implements Command {
 			.password(password)
 			.build();
 
+		/*
+		insertArticle 성공시 게시글 번호 반환
+		 */
 		int result = articleDAO.insertArticle(article);
+
+		if (result != 0) {
+			String fileSavePath = absolutePath+"\\"+result;
+			Path tempFile = Paths.get(tempPath + "\\" + fileName);
+			Path saveFile = Paths.get(fileSavePath);
+			Files.createDirectories(saveFile);
+			Files.move(tempFile,saveFile.resolve(tempFile.getFileName()));
+
+			FileVO file = FileVO.builder()
+				.articleId(result)
+				.name(fileName)
+				.path(fileSavePath)
+				.build();
+			FileDAO fileDAO = new FileDAO();
+			result = fileDAO.insertFile(file);
+		}
 
 		if (result == 1){
 			return "articleInserted.jsp";
 		} else {
-			//request set attribute
-			return "articleInputForm.jsp";
+			return "articleNotInserted.jsp";
 		}
 	}
 
