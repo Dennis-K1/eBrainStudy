c02ec68c94604b9cf6e044ad6915f990fcfb4375
Dennis-K1 <dennis.k1.dev@gmail.com>
2022-12-29 03:03:48 +0900

    Separate SearchParameters HashMap to each field variable

--- week3/src/main/java/com/week3/vo/SearchVO.java
+++ week3/src/main/java/com/week3/vo/SearchVO.java
@@ -1,11 +1,17 @@
 package com.week3.vo;
 
 import java.util.HashMap;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@ToString
 public class SearchVO {
 
 	/**
@@ -16,33 +22,38 @@ public class SearchVO {
 	/**
 	 * 페이징을 위한 특정 페이지 첫 행 인덱스
 	 */
-	private int firstArticleIndex;
+	private int firstArticleIndex = this.pageSize * (this.pageNumber - 1);
 
-	/**
-	 * 검색값 정보
-	 * Key (Value) :
-	 * 		startDate ('yyyy-mm-dd')
-	 * 		endDate ('yyyy-mm-dd')
-	 * 		category (article_category_id)
-	 * 		keyword (any text)
-	 */
-	private HashMap<String,String> searchParameters;
+//	/**
+//	 * 검색값 정보
+//	 * Key (Value) :
+//	 * 		startDate ('yyyy-mm-dd')
+//	 * 		endDate ('yyyy-mm-dd')
+//	 * 		category (article_category_id)
+//	 * 		keyword (any text)
+//	 */
+//	private HashMap<String,String> searchParameters;
 
+	private String startDate;
+	private String endDate;
+	private String category;
+	private String keyword;
+	private int pageNumber;
 
-	/**
-	 * 한 페이지 표시 게시글 수(pageSize)와 현재 페이지 번호 (pageNumber)를 이용해
-	 * 페이지 첫 번째 게시글 인덱스 지정
-	 * @param pageNumber 현재 페이지 번호
-	 */
-	public void setFirstArticleIndex(int pageNumber) {
-		this.firstArticleIndex = this.pageSize * (pageNumber - 1);
-	}
+//	/**
+//	 * 한 페이지 표시 게시글 수(pageSize)와 현재 페이지 번호 (pageNumber)를 이용해
+//	 * 페이지 첫 번째 게시글 인덱스 지정
+//	 * @param pageNumber 현재 페이지 번호
+//	 */
+//	public void setFirstArticleIndex(int pageNumber) {
+//		this.firstArticleIndex = this.pageSize * (pageNumber - 1);
+//	}
 
-	/**
-	 * 검색 조건이 든 HashMap 받아 저장
-	 * @param searchParameters null 값이 아닌 검색 조건이 담긴 HashMap
-	 */
-	public void setSearchParameters(HashMap<String, String> searchParameters) {
-		this.searchParameters = searchParameters;
-	}
+//	/**
+//	 * 검색 조건이 든 HashMap 받아 저장
+//	 * @param searchParameters null 값이 아닌 검색 조건이 담긴 HashMap
+//	 */
+//	public void setSearchParameters(HashMap<String, String> searchParameters) {
+//		this.searchParameters = searchParameters;
+//	}
 }
